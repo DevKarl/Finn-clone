511899e4eeafcda375077ea78f19ce3aede1f2ea
DevKarl <karl-dev@karl-dev.com>
2023-06-20 11:05:43 +0200

    categories icon svgs created, in assets

---
+++ Finn-clone/src/assets/home/categories/CategoriesIcons.tsx
@@ -0,0 +1,226 @@
+
+export const TorgetIcon = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="42" height="28" fill="none" viewBox="0 0 42 28">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+            d="M6 11V9a7 7 0 017-7h8m15 9V9a7 7 0 00-7-7h-8m15 9h-1a2 2 0 00-2 2v5H21m15-7h1a2 2 0 012 
+            2v5a6 6 0 01-6 6h-1m0 0H10m22 0v3m-22-3H9a6 6 0 01-6-6v-5a2 2 0 012-2h2a2 2 0 012 2v5h12m-11 
+            6v3m11-9V2">
+            </path>
+        </svg>
+    )
+}
+
+export const EiendomIcon = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="42" height="28" fill="none" viewBox="0 0 42 28">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+            d="M9 13L24 1l15 12m-3 14H12m24 0h5m-5 0V5h-4v2.4M12 27V10.6M12 27H5a4 4 0 114-4h1a2 2 0 
+            012 2v2zm15-1v-6a1 1 0 00-1-1h-4a1 1 0 00-1 1v6m0-15.75v2.5c0 .69.576 1.25 1.286 
+            1.25h3.428c.71 0 1.286-.56 1.286-1.25v-2.5C27 9.56 26.424 9 25.714 9h-3.428C21.576 9 21 
+            9.56 21 10.25z">
+            </path>
+        </svg>
+    )
+}
+
+export const BilOgNæringIcon = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="42" height="28" fill="none" 
+        viewBox="0 0 42 28">
+            <path fill="currentColor" d="M31.4 7.2l-.8.6.8-.6zM41 
+            16h1v-.535l-.445-.297L41 16zm0 7v1a1 1 0 001-1h-1zM5 24a1 1 0 100-2v2zm31-2a1 
+            1 0 100 2v-2zM2 4V3a1 1 0 00-.857 1.514L2 4zm24 8l-.8.6.3.4h.5v-1zM3.5 6.5l.933.359a1 
+            1 0 00-.076-.873L3.5 6.5zM1 13l-.933-.359A1 1 0 000 13h1zm22-5l.8-.6A1 1 0 0023 
+            7v1zm-8-1a1 1 0 100 2V7zM5 14a1 1 0 100-2v2zm25.6-6.2l3.6 4.8 1.6-1.2-3.6-4.8-1.6 
+            1.2zM40 16v7h2v-7h-2zM3 24h2v-2H3v2zm32-13h-9v2h9v-2zm6.555 4.168l-6-4-1.11 1.664 
+            6 4 1.11-1.664zM1.143 4.515l1.5 2.5 1.714-1.03-1.5-2.5-1.714 1.03zM2.567 6.14l-2.5 
+            6.5 1.866.718 2.5-6.5-1.866-.718zM26.8 11.4l-3-4-1.6 1.2 3 4 1.6-1.2zM23 7h-8v2h8V7zM1 
+            14h4v-2H1v2zm-1 7a3 3 0 003 3v-2a1 1 0 01-1-1H0zM32.2 6.6A9 9 0 0025 3v2a7 7 0 015.6
+            2.8l1.6-1.2zM35 22.5a3.5 3.5 0 01-3.5 3.5v2a5.5 5.5 0 005.5-5.5h-2zM31.5 26a3.5 3.5 
+            0 01-3.5-3.5h-2a5.5 5.5 0 005.5 5.5v-2zM28 22.5a3.5 3.5 0 013.5-3.5v-2a5.5 5.5 0 
+            00-5.5 5.5h2zm3.5-3.5a3.5 3.5 0 013.5 3.5h2a5.5 5.5 0 00-5.5-5.5v2zM13 22.5A3.5 3.5 
+            0 019.5 26v2a5.5 5.5 0 005.5-5.5h-2zM9.5 26A3.5 3.5 0 016 22.5H4A5.5 5.5 0 009.5 
+            28v-2zM6 22.5A3.5 3.5 0 019.5 19v-2A5.5 5.5 0 004 22.5h2zM9.5 19a3.5 3.5 0 013.5 
+            3.5h2A5.5 5.5 0 009.5 17v2zM28 22H14v2h14v-2zM0 13v8h2v-8H0zm41 9h-5v2h5v-2zM25 
+            3H2v2h23V3z">
+            </path>
+        </svg>
+    )
+}
+
+export const JobbIcon = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="42" height="28" fill="none" viewBox="0 0 42 28">
+            <path fill="currentColor" d="M39 12a1 1 0 10-2 0h2zM5 12a1 1 0 10-2 
+            0h2zm1-6h9V4H6v2zm10-1V3h-2v2h2zm1-3h8V0h-8v2zm-2 4h12V4H15v2zm12 
+            0h9V4h-9v2zm-1-3v2h2V3h-2zm-1-1a1 1 0 011 1h2a3 3 0 00-3-3v2zM3 24a3 3 0 
+            003 3v-2a1 1 0 01-1-1H3zm34 0a1 1 0 01-1 1v2a3 3 0 003-3h-2zM36 6a1 1 0 011 
+            1h2a3 3 0 00-3-3v2zM16 3a1 1 0 011-1V0a3 3 0 00-3 3h2zM6 4a3 3 0 00-3 3h2a1 1 
+            0 011-1V4zm26 15a7 7 0 007-7h-2a5 5 0 01-5 5v2zm-22-2a5 5 0 01-5-5H3a7 7 0 007 
+            7v-2zm9 5h4v-2h-4v2zm4-8h-4v2h4v-2zm0 2h2a2 2 0 00-2-2v2zm0 6a2 2 0 
+            002-2h-2v2zm-4-2h-2a2 2 0 002 2v-2zm0-6a2 2 0 00-2 2h2v-2zM5 24V7H3v17h2zM37 
+            7v17h2V7h-2zm-1 18H6v2h30v-2zm-13-6h9v-2h-9v2zm-13 0h9v-2h-9v2zm15 
+            1v-4h-2v4h2zm-8-4v4h2v-4h-2z">
+            </path>
+        </svg>
+    )
+}
+
+export const ReiseIcon = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="42" height="28" fill="none" viewBox="0 0 42 28">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+             d="M15.5 13h-5.142a3.844 3.844 0 01-1.97-.483 4.057 4.057 0 01-1.49-1.431L1 1h3.899l5.459 
+             4H37a4 4 0 010 8H27M9 9H2m13 4l2.571-2H27v4h-9.429L15 13zm16 14a3 3 0 112.152-5.09A3.999 
+             3.999 0 1137 27h-6zM7 18a2.999 2.999 0 00-2.933 2.356A1.943 1.943 0 003 20a2 2 0 100 
+             4h4a3 3 0 000-6z">
+            </path>
+        </svg>
+    )
+}
+
+export const BåtIcon = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="42" height="28" fill="none" viewBox="0 0 42 28">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+            d="M35 25l3.638-7.276a.5.5 0 00-.447-.724H3.809a.5.5 0 00-.447.724L7 25m-6 1.919a8.997 8.997
+             0 004.302-.47A9.682 9.682 0 009 24a8.158 8.158 0 002.717 2.157 7.493 7.493 0 
+             003.283.763c1.13 0 2.25-.26 3.283-.763A8.158 8.158 0 0021 24a8.157 8.157 0 002.717 2.156
+              7.494 7.494 0 003.283.763c1.13 0 2.25-.26 3.283-.763A8.157 8.157 0 0033 24a9.681 9.681 
+              0 003.698 2.45 8.998 8.998 0 004.302.469M13 2l-2 2-2-2M7 8l-2 2-2-2m18 9v-4m0 0h14L21 
+              1v12z">
+            </path>
+        </svg>
+    )
+}
+
+export const MCIcon = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="42" height="28" fill="none" viewBox="0 0 42 28">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+            d="M36 7.5V10M5 8h5.5l2.5 3m4 5v5h6l4-8 6-2.5V7m-16 9l-4-5m4 5l-4.5 2m.5-7h7l1.447-2.894A2
+             2 0 0123.237 7H33m0 0l-7-5m6 13.5L30 12m11 9a6 6 0 11-12 0 6 6 0 0112 0zm-28 0a6 6 
+             0 11-12 0 6 6 0 0112 0z">
+            </path>
+        </svg>
+    )
+}
+
+export const FeriehusOgHytterIcon = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="42" height="28" fill="none" viewBox="0 0 42 28">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+            d="M22 3l-2 2-2-2M1 27h40M4 14v13m16-13v13m-5 0v-7a1 1 0 00-1-1h-4a1 1 0 00-1 1v7M1 16l11-9 
+            11 9m10 4h6l-3-5.5h3l-3.5-6h3L33 1l-5.3 7.5h3l-3.7 6h3L27 20h6zm0 0v7">
+            </path>
+        </svg>
+    )
+}
+
+export const NybruktMobilIcon = () => {
+    return(
+        <svg width="42" height="28" viewBox="0 0 42 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_4530_106117)">
+            <path 
+            d="M21.5 3.5L21.0937 2.8906C20.7228 2.3342 20.0983 2 19.4296 2H7C5.89543 2 5 
+            2.89543 5 4V25C5 26.1046 5.89543 27 7 27H20C21.1046 27 22 26.1046 22 25V22" 
+            stroke="currentColor" stroke-width="2" stroke-linecap="round">
+            </path>
+            <path d="M6 23H21" stroke="currentColor" stroke-width="2" stroke-linecap="round" 
+            stroke-linejoin="round">
+            </path>
+            <path d="M12 6L15 6" stroke="currentColor" stroke-width="2.75" stroke-linecap="round">
+            </path>
+            <path d="M27.6931 4.58803C28.3702 3.76533 29.6298 3.76533 30.3069 4.58803L31.1823 
+            5.65181C31.5405 6.08703 32.0918 6.31539 32.6528 6.2609L34.0241 6.12773C35.0845 
+            6.02474 35.9753 6.91547 35.8723 7.97595L35.7391 9.34718C35.6846 9.90819 35.913 
+            10.4595 36.3482 10.8177L37.412 11.6931C38.2347 12.3702 38.2347 13.6298 37.412 
+            14.3069L36.3482 15.1823C35.913 15.5405 35.6846 16.0918 35.7391 16.6528L35.8723 
+            18.0241C35.9753 19.0845 35.0845 19.9753 34.0241 19.8723L32.6528 19.7391C32.0918 
+            19.6846 31.5405 19.913 31.1823 20.3482L30.3069 21.412C29.6298 22.2347 28.3702 
+            22.2347 27.6931 21.412L26.8177 20.3482C26.4595 19.913 25.9082 19.6846 25.3472 
+            19.7391L23.9759 19.8723C22.9155 19.9753 22.0247 19.0845 22.1277 18.0241L22.2609 
+            16.6528C22.3154 16.0918 22.087 15.5405 21.6518 15.1823L20.588 14.3069C19.7653 
+            13.6298 19.7653 12.3702 20.588 11.6931L21.6518 10.8177C22.087 10.4595 22.3154 
+            9.90819 22.2609 9.34718L22.1277 7.97595C22.0247 6.91547 22.9155 6.02474 23.9759 
+            6.12773L25.3472 6.2609C25.9082 6.31539 26.4595 6.08703 26.8177 5.65181L27.6931 
+            4.58803Z" stroke="currentColor" stroke-width="2">
+            </path>
+            </g>
+            <path d="M26 13.5L28.0211 15.3045C28.4474 15.6852 29.104 15.6381 29.4716 15.2004L33 11" 
+            stroke="currentColor" stroke-width="2" stroke-linecap="round">
+            </path>
+            <defs>
+                <clipPath id="clip0_4530_106117">
+                    <rect width="42" height="28" fill="white"></rect>
+                </clipPath>
+            </defs>
+        </svg>
+    )
+}
+
+export const BilabonnementIcon = () => {
+    return(
+        <svg width="28" height="27" viewBox="0 0 28 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M21.1321 0C17.6152 0 14.7642 2.85103 14.7642 6.36792C14.7642 9.88482 17.6152 
+            12.7358 21.1321 12.7358C24.649 12.7358 27.5 9.88482 27.5 6.36792C27.5 2.85103 24.6488 
+            0 21.1321 0ZM21.1321 9.64351C19.3231 9.64351 17.8566 8.17693 17.8566 6.36807C17.8566 
+            4.55906 19.3231 3.09263 21.1321 3.09263C22.9409 3.09263 24.4075 4.55906 24.4075 
+            6.36807C24.4075 8.17693 22.9409 9.64351 21.1321 9.64351Z" fill="#570E2D">
+            </path>
+            <path d="M10.1435 14.2637V22.5099L4.46351 14.2637H0.5V26.9997H3.59248V17.4889L10.1435 
+            26.9997H13.236V14.2637H10.1435Z" fill="#570E2D">
+            </path>
+            <path d="M23.8981 26.9997H27.4999L22.3519 19.6278L27.1485 14.2637H23.652L17.8564 
+            21.0037V14.2637H14.7639V26.9997H17.8564V24.655L20.3293 21.8895L23.8981 26.9997Z" 
+            fill="#570E2D">
+            </path>
+            <path d="M10.1435 
+            0V4.99092H3.59248V0H0.5V12.736H3.59248V7.74507H10.1435V12.736H13.236V0H10.1435Z" 
+            fill="#570E2D">
+            </path>
+        </svg>
+    )
+}
+
+export const NettbilIcon = () => {
+    return(
+        <svg width="45" height="35" viewBox="0 0 45 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="45" height="35" fill="white"></rect>
+            <path d="M6.65422 24.347C4.4512 22.1439 4.4512 18.5701 6.65422 16.3671C8.85724 
+            14.1641 12.431 14.1641 14.6341 16.3671C16.8371 18.5701 16.8371 22.1439 14.6341 
+            24.347C12.431 26.55 8.85724 26.55 6.65422 24.347Z" fill="#004046">
+            </path>
+            <path d="M29.3593 24.347C27.1563 22.1439 27.1563 18.5701 29.3593 16.3671C31.5623 
+            14.1641 35.1361 14.1641 37.3392 16.3671C39.5422 18.5701 39.5422 22.1439 37.3392 
+            24.347C35.1361 26.55 31.5623 26.55 29.3593 24.347Z" fill="#004046">
+            </path>
+            <path d="M33.3499 20.3578C33.3499 14.0963 28.2585 9 21.997 9C15.7355 9 10.6392 14.0963 
+            10.6392 20.3578H33.3499Z" fill="#FF8752">
+            </path>
+        </svg>
+    )
+}
+
+export const MittAnbudIcon = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="35" viewBox="0 0 100 100" fill="none">
+            <path fill="#101F41" d="M36.002 7.003.005 43l10 10 35.997-35.997-10-10Z">
+            </path>
+            <path fill="#3960BF" d="M32.004 39a14.145 14.145 0 0 0 19.999 0l11.999-11.999L90 
+            53 100 43 64.002 7.002 32.004 39.001Z">
+            </path>
+            <path fill="#101F41" d="M56.002 43a19.799 19.799 0 0 1-27.998 0l-14 14 35.999 
+            35.998 6-6-14-14 4-3.999 14 14 5.999-6-14-14 4-4 14 14 6-6-14-14 4-3.999 14 14L86 
+            57 64 35l-7.999 8Z">
+            </path>
+        </svg>
+    )
+}
+
+
+
+
+
+
