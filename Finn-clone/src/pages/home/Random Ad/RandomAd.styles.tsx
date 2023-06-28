a03bb6334ebedadd9cfdeb3a0355e4a7b65680b7
DevKarl <karl-dev@karl-dev.com>
2023-06-29 00:05:45 +0200

    random ad in the works

--- Finn-clone/src/pages/home/Random Ad/RandomAd.styles.tsx
+++ Finn-clone/src/pages/home/Random Ad/RandomAd.styles.tsx
@@ -1,21 +1,55 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const StyledRandomAdWrapper = styled.section`
 
-    height: 192px;
-    background-color: skyblue;
     display: flex;
     flex-direction: row;
     margin: 16px;
     border-radius: 8px;
-    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+
+
+    @media (max-width: 480px) {
+
+
+
+
+    }
+
+
 `
 
 export const StyledDescription = styled.div`
-    background-color: antiquewhite;
     border-top-left-radius: 8px;
     border-bottom-left-radius: 8px;
+    padding: 10px;
     flex: 1.5;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    justify-content: center;
+    > h2 {
+        font-family: inherit;
+        font-size: 1.5em;
+        margin-bottom: 5px;
+    }
+
+    > p {
+        margin-bottom: 5px;
+    }
+`
+
+export const StyledLink = styled(Link)`
+    margin-bottom: 5px;
+    background-color: #0063fb;
+    color: white;
+    text-decoration: none;
+    padding: 8px 16px;
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 16px;
+    border-radius: 4px;
 `
 
 export const StyledImgWrapper = styled.div`
--- Finn-clone/src/pages/home/Random Ad/RandomAd.tsx
+++ Finn-clone/src/pages/home/Random Ad/RandomAd.tsx
@@ -1,10 +1,16 @@
-import { StyledRandomAdWrapper, StyledDescription, StyledImgWrapper, StyledImg } from "./RandomAd.styles";
+import { StyledRandomAdWrapper, StyledDescription, StyledImgWrapper, StyledImg, StyledLink } from "./RandomAd.styles";
 
 const RandomAd = () => {
 
     return(
         <StyledRandomAdWrapper>
-            <StyledDescription/>
+            <StyledDescription>
+                <h2>Skjærgårdsidyll i sør</h2>
+                <p>Krabbefiske, saltvannshår og bløde konsonanter – hva med Sørlandet i år?</p>
+                <StyledLink to="/">
+                Se feriehus på Sørlandet
+                </StyledLink>
+            </StyledDescription>
             <StyledImgWrapper>
                 <StyledImg src="https://assets.finn.no/pkg/frontpage-podium/2.0.93/travel-feriehus-sorlandet.png"/>
             </StyledImgWrapper>
