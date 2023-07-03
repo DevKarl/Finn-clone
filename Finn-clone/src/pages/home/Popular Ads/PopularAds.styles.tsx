2e5e7bde288e2cd50aee1a75633849b1b7765105
DevKarl <karl-dev@karl-dev.com>
2023-07-03 22:55:23 +0200

    testing public API for dummy products

---
+++ Finn-clone/src/pages/home/Popular Ads/PopularAds.styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.section`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-auto-rows: 1fr;
+    gap: 22px;
+`
+
+export const StyledAdWrapper = styled.div`
+    height: 200px;
+    border-radius: 10px;
+`
--- Finn-clone/src/pages/home/Popular Ads/PopularAds.tsx
+++ Finn-clone/src/pages/home/Popular Ads/PopularAds.tsx
@@ -1,11 +1,51 @@
+import { useEffect, useState } from "react";
+import { StyledContainer, StyledAdWrapper } from "./PopularAds.styles";
 
-import ProductGrid from "../../../components/product components/ProductGrid";
-
-const PopularAds = () => {
-    return(
-        <ProductGrid></ProductGrid>
-    )
+interface Ad {
+    brand: string;
+    category: string;
+    description: string;
+    discountPercentage: number;
+    id: number;
+    images: string[];
+    price: number;
+    rating: number;
+    stock: number;
+    thumbnail: string;
+    title: string;
 }
 
+const PopularAds = () => {
+
+  const [adData, setData] = useState<Ad[]>([]);
+
+  useEffect(() => {
+    const getAds = async function () {
+      const response = await fetch("https://dummyjson.com/products");
+      const data = await response.json();
+      console.log(data);
+      setData(data.products);
+    };
+    getAds();
+  }, []);
+
+  return (
+    <>
+      <h1>Populære annonser</h1>
+      <StyledContainer>
+        {adData.map(ad => (
+          <StyledAdWrapper key={ad.id}>
+            <img src={ad.images[0]} style={{width: '100px'}}></img>
+            <div>
+              <p>place</p>
+              <h3>{ad.title}</h3>
+              <p>{ad.price}</p>
+            </div>
+          </StyledAdWrapper>
+        ))}
+      </StyledContainer>
+    </>
+  );
+};
 
 export default PopularAds;
